36b43e534948c5960689db2f8baa9c6df405ab72
marie <marie@getacademy.no>
2024-11-21 14:00:24 +0100

    Checkin

---
+++ Hundehviskeren/Hundehviskeren/Dog.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hundehviskeren
+{ 
+    //Hva kan beskrive en hund
+    //hva kan en hund eie
+    //Hva kan en hund gjøre
+    //løpe, leke, spise, sove, bjeffe, destroyFurniture, bite,legHump
+    //Pause til 13.00
+    internal class Dog
+    {
+        string color;
+        string race;
+        string size;
+        string[] toys;
+        public int age;
+        string name;
+        string gender;
+        int hunger;
+        int satisfaction;
+
+        public Dog()
+        {
+
+        }
+        public Dog(string startName, int startAge)
+        {
+            name = startName;
+            age = startAge;
+        }
+
+        public void SetAge(int newAge)
+        {
+            age = newAge;
+        }
+        public void Play()
+        {
+            Console.WriteLine($"{name} is playing");
+            satisfaction++;
+        }
+        public void Eat()
+        {
+            Console.WriteLine($"{name} is eating");
+            hunger++;
+        }
+        public void Sleep()
+        {
+
+        }
+        public void Hump(string humpObject)
+        {
+            Console.WriteLine($"{name} is humping {humpObject}");
+            satisfaction += 30;
+        }
+        public void StareIntoTHeAbyssInCorner()
+        {
+            //Dedikert til Linn
+            Console.WriteLine($"{name} is staring into the abyss while sitting in corner and pondering life desicions");
+        }
+        public void Bark()
+        {
+
+        }
+    }
+}
---
+++ Hundehviskeren/Hundehviskeren/Program.cs
@@ -0,0 +1,83 @@
+﻿//Menybasert console app
+//Intro til objekter - JS/ vs c# objekt
+//Hundehviskeren - 
+/*
+ let dog = {
+color: 'black',
+race: 'pitbull',
+size: 'large',
+toys: [],
+age: 10,
+name: 'Fido',
+sex: 'female'
+}
+*/
+
+//Pause til 13.30
+using Hundehviskeren;
+
+int age = 0;
+//int[] numbers = new int[10];
+//Dog[] dogs = new Dog[10];
+
+Dog bjarne = new Dog("bjarne", 3);
+Dog kåre = new Dog("kåre", 1);
+
+//List<Dog> dogsInApp = new List<Dog>() { bjarne,kåre, new Dog("truls",5)};
+
+//Dog chosenDog = new Dog();
+bool isRunning = true;
+while (isRunning)
+{
+    Menu();
+}
+
+void Menu()
+{
+    Console.WriteLine("Welcome to the dog park");
+   // Console.WriteLine("1. Choose dog");
+    Console.WriteLine("2. Feed dog");
+    Console.WriteLine("3. Play with dog");
+    Console.WriteLine("4. Show cupcake");
+    Console.WriteLine("5. exit app");
+
+    var input = Console.ReadLine();
+    Console.WriteLine($"brukeren tastet {input}");
+
+    switch (input)
+    {
+        case "2":
+            bjarne.Eat();
+            break;
+        case "3":
+            bjarne.Play();
+            break;
+        case "4":
+            kåre.StareIntoTHeAbyssInCorner();
+            break;
+        case "5":
+            isRunning = false;
+            break;
+        default:
+            Console.WriteLine("Incorrect choice. Shutting down");
+            Environment.Exit(0);
+            break;
+
+    }
+
+ 
+}
+
+void chooseDog()
+{
+    Console.WriteLine("Do you want to choose Bjarne? Y/N");
+    var answer = Console.ReadLine();
+    if (answer == "Y")
+    {
+        //chosenDog = bjarne;
+    }
+    else
+    {
+       // chosenDog = kåre;
+    }
+}
